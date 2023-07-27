eb0fddaa6da85f46d0afefc3db118d155e97ba22
Saky-Dev <garciaisaac468@gmail.com>
2023-07-27 15:18:40 -0600

    feat: initialized home page
    created pages' directory and
    home index file

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,15 @@
+import './index.sass'
+
+const Home = () => {
+  return (
+    <main>
+      <section className='almost' />
+      <section className='full' />
+      <section className='almost' />
+      <section className='full' />
+      <section className='almost' />
+    </main>
+  )
+}
+
+export default Home
